27873776c7a2af0e6d76ee8fb158fc2550652710
Sam Hart <contact@justsam.io>
2023-12-31 03:44:30 +0000

    refactor(types): clean up avg_empty_re... setup

--- tests/numeric_safety_tests.rs
+++ tests/numeric_safety_tests.rs
@@ -9,10 +9,9 @@ fn create_test_storage() -> (StorageEngine, TempDir) {
     let mut config = Config::default();
     config.storage.data_dir = temp.path().to_path_buf();
     config.storage.performance.num_threads = 2;
-    let storage = StorageEngine::new(config.clone()).unwrap();
+    let storage = StorageEngine::new(config).unwrap();
     (storage, temp)
 }
-
 
 // AVG Aggregation Tests
 #[test]
@@ -51,6 +50,66 @@ fn test_avg_multiple_values() {
         .unwrap();
     // Should have a result (not crash)
     // The actual value should be around 20.0
+}
+
+#[test]
+fn test_avg_empty_relation_no_panic() {
+    let (mut storage, _temp) = create_test_storage();
+
+    storage.create_knowledge_graph("test_avg_empty").unwrap();
+    storage.use_knowledge_graph("test_avg_empty").unwrap();
+
+    // Create empty relation by declaring schema
+    // Note: schema declaration syntax depends on implementation
+    // For now, just verify empty query doesn't panic
+
+    // Query on nonexistent relation should not crash
+    // Either returns empty results or error (depending on implementation)
+    let _result = storage.execute_query("result(avg<V>) :- nonexistent(_, V).");
+    // Test passes if no panic occurred
+}
+
+// COUNT Aggregation Tests
+#[test]
+fn test_count_empty_returns_zero_or_empty() {
+    let (mut storage, _temp) = create_test_storage();
+
+    storage.create_knowledge_graph("test_count").unwrap();
+    storage.use_knowledge_graph("test_count").unwrap();
+
+    // Query count on nonexistent relation
+    // Should not panic - either returns empty or 0
+    let _result = storage.execute_query("result(count<X>) :- nonexistent(X).");
+    // Test passes if no panic occurred
+}
+
+#[test]
+fn test_count_single_value() {
+    let (mut storage, _temp) = create_test_storage();
+
+    storage.create_knowledge_graph("test_count").unwrap();
+    storage.use_knowledge_graph("test_count").unwrap();
+
+    storage.insert("items", vec![(1, 0)]).unwrap();
+
+    let _results = storage
+        .execute_query("result(count<X>) :- items(X).")
+        .unwrap();
+    // COUNT should return a result (1 for single item)
+    // Note: Exact behavior depends on aggregation implementation
+}
+
+// SUM Aggregation Tests
+#[test]
+fn test_sum_empty_returns_zero_or_empty() {
+    let (mut storage, _temp) = create_test_storage();
+
+    storage.create_knowledge_graph("test_sum").unwrap();
+    storage.use_knowledge_graph("test_sum").unwrap();
+
+    // Should not panic
+    let _result = storage.execute_query("result(sum<V>) :- nonexistent(_, V).");
+    // Test passes if no panic occurred
 }
 
 #[test]
